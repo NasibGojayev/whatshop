bb11aec18e33d451c682058f984e2f51c8bdc916
Nasib Gojayev <nqocayev2006@gmail.com>
2025-02-05 00:00:10 +0400

    only cackage

--- lib/deletable/categories_provider.dart
+++ lib/deletable/categories_provider.dart
@@ -1,5 +1,5 @@
 /*
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'cackage:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
 class CategoryProvider with ChangeNotifier{
